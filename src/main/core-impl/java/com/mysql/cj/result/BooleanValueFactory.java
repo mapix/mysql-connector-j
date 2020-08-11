e71b178eff193315a1cf2863a445a056346a15c8
mapix <mapix.me@gmail.com>
2020-08-11 19:24:14 +0800

    add more value for bool

--- src/main/core-impl/java/com/mysql/cj/result/BooleanValueFactory.java
+++ src/main/core-impl/java/com/mysql/cj/result/BooleanValueFactory.java
@@ -98,9 +98,9 @@ public class BooleanValueFactory extends DefaultValueFactory<Boolean> {
         String s = StringUtils.toString(bytes, offset, length, f.getEncoding());
         byte[] newBytes = s.getBytes();
 
-        if (s.equalsIgnoreCase("Y") || s.equalsIgnoreCase("true")) {
+        if (s.equalsIgnoreCase("Y") || s.equalsIgnoreCase("YES") || s.equalsIgnoreCase("true")) {
             return createFromLong(1);
-        } else if (s.equalsIgnoreCase("N") || s.equalsIgnoreCase("false")) {
+        } else if (s.equalsIgnoreCase("N") || s.equalsIgnoreCase("NO") || s.equalsIgnoreCase("false")) {
             return createFromLong(0);
         } else if (s.contains("e") || s.contains("E") || s.matches("-?\\d*\\.\\d*")) {
             // floating point
